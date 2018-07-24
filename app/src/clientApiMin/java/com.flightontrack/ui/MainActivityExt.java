6d502dff8fcb50df81ce374851dc235c139c8cd2
Tikhomirov <flightontrack@gmail.com>
2018-07-24 17:09:40 -0400

    7/24/18

--- app/src/clientApiMin/java/com.flightontrack/ui/MainActivityExt.java
+++ app/src/clientApiMin/java/com.flightontrack/ui/MainActivityExt.java
@@ -1,18 +1,6 @@
 package ui;
 
-import android.content.ActivityNotFoundException;
-import android.content.Context;
-import android.content.Intent;
 import android.view.MenuItem;
-import android.widget.Toast;
-
-import com.flightontrack.R;
-import com.flightontrack.flight.RouteBase;
-import com.flightontrack.ui.FaceBookActivity;
-import com.flightontrack.ui.PrivacyPolicyActivity;
-
-import static com.flightontrack.shared.Props.ctxApp;
-import static com.flightontrack.shared.Props.mainactivityInstance;
 
 public class MainActivityExt {
     static final String TAG = "MainActivity";
